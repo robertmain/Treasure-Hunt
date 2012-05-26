73d20bdc8b6087578c4f0dd65df71d01659aabf2
lenswipe@gmail.com <lenswipe@gmail.com>
2012-05-26 16:27:19 +0100

    Made site EU cookie compliant and renamed codeigniter login cookie

--- application/config/config.php
+++ application/config/config.php
@@ -247,7 +247,7 @@ $config['encryption_key'] = 'mgYcJMJ\/4Zl$0x-iQQ70$0wrDYCHCL\tuV27p&7sUrkdeiB%,H
   | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
   |
  */
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = hash('md5',APPTITLE);
 $config['sess_expiration'] = 7200;
 $config['sess_expire_on_close'] = FALSE;
 $config['sess_encrypt_cookie'] = TRUE;
--- application/views/templates/template.php
+++ application/views/templates/template.php
@@ -6,6 +6,8 @@
         <script type="text/javascript" src="<?= base_url() . VIEWPATH . 'js/jquery.min.js' ?>"></script>
         <script type="text/javascript" src="<?= base_url() . VIEWPATH . 'js/bootstrap.min.js' ?>"></script>
         <meta name="viewport" content="user-scalable=no, width=device-width" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <title><?= APPTITLE ?></title>
         <script type="text/javascript">
             window.addEventListener("load",function() {
@@ -16,6 +18,20 @@
         </script>
     </head>
     <body>
+        <div class="modal fade hide in out" id="myModal">
+            <div class="modal-header">
+                <h3>Cookies On <?= APPTITLE ?></h3>
+            </div>
+            <div class="modal-body">
+                <p><?= APPTITLE ?> uses cookies to keep your login session active.
+                    By clicking "Dismiss" and continuing to use this application, we assume that you are happy for us to continue to use cookies in this manner</p>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="btn btn-success dismiss">Dismiss</a>
+                <a href="http://www.google.com" class="btn btn-danger">Leave Application</a>
+            </div>
+        </div>
+
         <div class="navbar">
             <div class="navbar-inner">
                 <div class="container">
@@ -84,3 +100,38 @@
         </div>
     </body>
 </html>
+<?php if ($this->agent->is_mobile()): ?>
+    <script type="text/javascript">
+        function setCookie(c_name,value,exdays){
+            var exdate=new Date();
+            exdate.setDate(exdate.getDate() + exdays);
+            var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+            document.cookie=c_name + "=" + c_value;
+        }
+        function getCookie(c_name){
+            var i,x,y,ARRcookies=document.cookie.split(";");
+            for (i=0;i<ARRcookies.length;i++)
+            {
+                x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+                y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+                x=x.replace(/^\s+|\s+$/g,"");
+                if (x==c_name)
+                {
+                    return unescape(y);
+                }
+            }
+        }
+            
+        var messageSeen = getCookie("messageSeen");
+        if(messageSeen == null){
+            $(document).ready(function() {
+                $('#myModal').modal('show')
+            });
+        }
+            
+        $('.dismiss').click(function(){
+            setCookie("messageSeen",true,365);
+            $('#myModal').modal('hide');
+        });
+    </script>
+<?php endif; ?>
