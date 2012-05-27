a8880466004d746fcca117ba3c871416dd6597b6
lenswipe@gmail.com <lenswipe@gmail.com>
2012-05-27 19:10:17 +0100

    Commented problematic line out

--- application/controllers/admin_home.php
+++ application/controllers/admin_home.php
@@ -15,7 +15,7 @@ class Admin_home extends Admin_Controller {
         $this->data['registeredUsers'] = $this->pirate_model->get_many_by('admin', '0');
         $this->data['totalCodesInDatabase'] = $this->treasure_model->get_all();
         $this->data['totalFoundCodes'] = $this->mytreasure_model->get_all();
-        $this->data['treasure_per_pirate'] = $this->mytreasure_model->treasure_per_pirate();
+        //$this->data['treasure_per_pirate'] = $this->mytreasure_model->treasure_per_pirate();
         $this->data['analytics'] = $this->mytreasure_model->getAnalyticsData();
         $this->template->write_view('content', 'views/admin/home/index', $this->data);
         $this->template->render();
