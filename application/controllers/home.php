ae0b3fd5f59e4de14b4570f2ec4da2ee60d3b702
lenswipe@gmail.com <lenswipe@gmail.com>
2012-05-30 11:57:37 +0100

    Added help button to home page and short description of app

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -7,11 +7,12 @@ class Home extends MY_Controller {
         if (isLoggedIn()) {
             redirect('treasure');
         }
-        $this->load->model(array('mytreasure_model'));
+        $this->load->model(array('mytreasure_model', 'treasure_model'));
     }
 
     public function index() {
-        $this->template->write_view('content', 'views/landing/index');
+        $this->data['amountOfTreasure'] = count($this->treasure_model->get_all());
+        $this->template->write_view('content', 'views/landing/index', $this->data);
         $this->template->render();
     }
 
