7b7759a90a45cecedc5e4a5ed41cde7face22a0b
Robert Main <lenswipe@gmail.com>
2019-10-12 18:38:21 -0400

    Lint Hooks

--- src/hooks/phpunit.php
+++ src/hooks/phpunit.php
@@ -4,11 +4,14 @@
  * CodeIgniter system hook(s) to enable the usage of PHPUnit for unit testing
  * with CodeIgniter
  */
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+// phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
 class PHPUnit_CodeIgniter_Hook
 {
     /**
-     * Pre system hook used to load the config and benchmark classes. 
+     * Pre system hook used to load the config and benchmark classes.
     */
+    // phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
     public function pre_system()
     {
         $GLOBALS['CFG'] =& load_class('Config', 'core');
