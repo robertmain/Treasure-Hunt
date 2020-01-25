1cf1ff62e122d3e0892bfc2907cfcc1baba55141
Robert Main <lenswipe@gmail.com>
2020-01-24 23:19:47 -0500

    Extract banned message into separate component for re-use

---
+++ src/views/partials/treasure/banned.php
@@ -0,0 +1,9 @@
+<div class="alert">
+    <h3 class="alert-heading">Information</h3>
+    <p>
+        Your account has been suspended. You may still use this application,
+        however you will be unable to find new pieces of treasure and (at the discretion of staff)
+        your existing pieces of treasure may be removed from you.
+        Please contact a member of <?= APP_OWNER ?> for more information.
+    </p>
+</div>
--- src/views/partials/treasure/index.php
+++ src/views/partials/treasure/index.php
@@ -4,17 +4,12 @@ $this->layout('layouts/default', [
 ]);
 ?>
 
-<?php if (isBanned($me->id) && isLoggedIn()) : ?>
-    <div class="alert">
-        <h3 class="alert-heading">Information</h3>
-        <p>
-            Your account has been suspended. You may still use this application,
-            however you will be unable to find new pieces of treasure and (at the discretion of staff)
-            your existing pieces of treasure may be removed from you.
-            Please contact a member of <?= APP_OWNER ?> for more information.
-        </p>
-    </div>
-<?php endif; ?>
+<?php
+if (isBanned($me->id) && isLoggedIn()) {
+    $this->insert('partials::treasure/banned');
+}
+?>
+
 <?php if (sizeof($treasure) > 0) : ?>
     <div id="foundAllModal" class="modal hide fade" style="display: none; ">
         <div class="modal-header">
